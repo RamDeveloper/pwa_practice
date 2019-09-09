57ffc0781f637273915f73e7ddf39c996fa26d7a
ramdeveloper <ram1489@gmail.com>
2019-09-09 17:41:50 +0530

    Initialize the basic Progressive web apps

---
+++ src/sw.js
@@ -0,0 +1,26 @@
+var cacheName = 'basic-pwa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/main.css'
+];
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
